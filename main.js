4dc3e39fde7104145f4b2fd6e77ee474000f7857
Csaba Valyi <csaba_valyi@epam.com>
2016-06-22 15:30:30 +0200

    node init

---
+++ main.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+
+const PORT = 3000;
+
+app.use(express.static(__dirname));
+
+app.listen(PORT, () => {
+    console.log(`Starting to listen on port: ${PORT}`);
+});
